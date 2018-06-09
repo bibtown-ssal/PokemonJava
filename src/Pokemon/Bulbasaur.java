1e5c6da645162cbce8ba236e38811c08382856a9
ssalogel <charlestherien@hotmail.com>
2018-06-08 22:56:28 -0400

    changed base stats handling for easier access on levelUp

--- src/Pokemon/Bulbasaur.java
+++ src/Pokemon/Bulbasaur.java
@@ -1,11 +1,25 @@
 package Pokemon;
 
+import java.util.HashMap;
+
 public class Bulbasaur extends Pokemon implements ExpMedSlow {
     protected static String species = "Bulbasaur";
     protected static String[] type1 = {"Grass","Poison"};
 
+    @Override
+    protected static HashMap<String, Integer> baseStats = new HashMap<>();
+
+    {
+        this.baseStats.put("health", 45);
+        this.baseStats.put("attack", 49);
+        this.baseStats.put("defense", 49);
+        this.baseStats.put("spAtt", 65);
+        this.baseStats.put("spDef", 65);
+        this.baseStats.put("speed", 45);
+    }
+
     public Bulbasaur(int level){
-        super(level,45,49,49,65,65,45, species);
+        super(level, species);
         this.gender = "";
     }
 
