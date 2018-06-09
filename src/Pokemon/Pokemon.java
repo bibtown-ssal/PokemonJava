5478c5747578cd22d4759e500a44fc0975a3ec8a
ssalogel <charlestherien@hotmail.com>
2018-06-08 22:57:19 -0400

    Put in a function for refreshStats, used better baseStats handling

--- src/Pokemon/Pokemon.java
+++ src/Pokemon/Pokemon.java
@@ -8,25 +8,23 @@ public abstract class Pokemon {
     protected int currentHP;
     private HashMap<String, Integer> stats = new HashMap<>();
     private HashMap<String, Integer> iv = new HashMap<>();
+    private HashMap<String, Integer> base = new HashMap<>();
     protected int experience = 0;
     protected int expToLvlUp = 0;
     private int EV = 0;
+    abstract protected HashMap<String, Integer> baseStats;
 
-    public Pokemon(int level, int health, int attack, int defence, int spAtt, int spDef, int speed, String name){
+    public Pokemon(int level, String name){
         this.iv.put("health", 1+(int)(Math.random()*31));
         this.iv.put("attack", 1+(int)(Math.random()*31));
-        this.iv.put("defence", 1+(int)(Math.random()*31));
+        this.iv.put("defense", 1+(int)(Math.random()*31));
         this.iv.put("spAtt", 1+(int)(Math.random()*31));
         this.iv.put("spDef", 1+(int)(Math.random()*31));
         this.iv.put("Speed", 1+(int)(Math.random()*31));
-        this.stats.put("health", calculateHealth(health));
-        this.stats.put("attack", calculateStat(attack));
-        this.stats.put("defence", calculateStat(defence));
-        this.stats.put("spAtt", calculateStat(spAtt));
-        this.stats.put("spDef", calculateStat(spDef));
-        this.stats.put("speed", calculateStat(speed));
+
         this.name = name;
         this.level = level;
+        refreshStats();
     }
 
     public String getName() {
@@ -47,7 +45,7 @@ public abstract class Pokemon {
 
     public String getStats() {
         String stats = "Health : " + this.stats.get("health") + "\nAttack : " + this.stats.get("attack") + "\nDefense : "
-                + this.stats.get("defence") + "\nSpecial Attack : " + this.stats.get("spAttack") + "\nSpecial Defence : "
+                + this.stats.get("defense") + "\nSpecial Attack : " + this.stats.get("spAttack") + "\nSpecial Defense : "
                 + this.stats.get("spDef") + "\nSpeed : " + this.stats.get("speed");
         return stats;
     }
@@ -58,7 +56,7 @@ public abstract class Pokemon {
 
     public String getIv() {
         String iv = "Health : " + this.iv.get("health") + "\nAttack : " + this.iv.get("attack") + "\nDefense : "
-                + this.iv.get("defence") + "\nSpecial Attack : " + this.iv.get("spAttack") + "\nSpecial Defence : "
+                + this.iv.get("defense") + "\nSpecial Attack : " + this.iv.get("spAttack") + "\nSpecial Defense : "
                 + this.iv.get("spDef") + "\nSpeed : " + this.iv.get("speed");
         return iv;
     }
@@ -94,11 +92,23 @@ public abstract class Pokemon {
         }
     }
 
-    public void levelUp(){
+    private void levelUp(){
         experience -= expToLvlUp;
         level ++;
         expToLvlUp = recalculateExp(this.level);
+
+        refreshStats();
     };
+
+    private void refreshStats(){
+        this.stats.put("health", calculateHealth());
+        this.stats.put("attack", calculateStat(this.baseStats.get("attack")));
+        this.stats.put("defense", calculateStat(this.baseStats.get("defense")));
+        this.stats.put("spAtt", calculateStat(this.baseStats.get("spAtt")));
+        this.stats.put("spDef", calculateStat(this.baseStats.get("spDef")));
+        this.stats.put("speed", calculateStat(this.baseStats.get("speed")));
+    }
+
     private int calculateHealth(int base){
      return (int)Math.floor(((base + this.iv.get("health"))*2+Math.floor(Math.sqrt(EV)/4))*this.level/100)+level+10;
     }
